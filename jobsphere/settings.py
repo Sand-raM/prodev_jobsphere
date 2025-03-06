b2469f3433b0a0318469bd81193f18771c70a5d1
SANDRA MURAZA <139123667+Sand-raM@users.noreply.github.com>
2025-03-06 17:57:56 +0200

    Update settings.py

--- jobsphere/settings.py
+++ jobsphere/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t@)+qnyvpp6mb#m9m#lplt@dnpldz3^bn7!mvksv9*a282*y+6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
+ALLOWED_HOSTS = ['prodev-jobsphere.onrender.com', '127.0.0.1', 'localhost', 'testserver']
 
 INTERNAL_IPS = ['127.0.0.1']
 
